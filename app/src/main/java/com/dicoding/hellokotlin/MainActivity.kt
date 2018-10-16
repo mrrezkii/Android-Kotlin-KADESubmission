20e987b72ec501f636a9936c4462fd5874d5a336
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2018-10-16 11:02:20 +0700

    Membuat method untuk memasukkan array

--- app/src/main/java/com/dicoding/hellokotlin/MainActivity.kt
+++ app/src/main/java/com/dicoding/hellokotlin/MainActivity.kt
@@ -11,4 +11,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    private fun initData() {
+        val name = resources.getStringArray(R.array.club_name)
+        val image = resources.obtainTypedArray(R.array.club_image)
+        items.clear()
+        for (i in name.indices) {
+            items.add(
+                Item(
+                    name[i],
+                    image.getResourceId(i, 0)
+                )
+            )
+        }
+
+        //Recycle the typed array
+        image.recycle()
+    }
 }
