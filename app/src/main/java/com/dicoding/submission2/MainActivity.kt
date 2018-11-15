d82aaa1cb3118d2307b5b65cace26432349a6413
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2018-11-15 14:47:53 +0700

    Membuat objek tab layout di Main Activity

--- app/src/main/java/com/dicoding/submission2/MainActivity.kt
+++ app/src/main/java/com/dicoding/submission2/MainActivity.kt
@@ -12,5 +12,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         viewpager_main.adapter = MyPagerAdapter(supportFragmentManager)
+        tabs_main.setupWithViewPager(viewpager_main)
     }
 }
