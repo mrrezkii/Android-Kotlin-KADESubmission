9baf973867b2fd2f2bfe71512654efdeb38ab3d6
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2018-11-15 14:38:00 +0700

    Membuat class MyPagerAdapter dan instalisasi

---
+++ app/src/main/java/com/dicoding/submission2/MyPagerAdapter.kt
@@ -0,0 +1,35 @@
+package com.dicoding.submission2
+
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentPagerAdapter
+import com.dicoding.submission2.Fragment.LastMatchFragment
+import com.dicoding.submission2.Fragment.NextMatchFragment
+
+class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    // sebuah list yang menampung objek Fragment
+    private val pages = listOf(
+        LastMatchFragment(),
+        NextMatchFragment()
+
+
+    )
+
+    // menentukan fragment yang akan dibuka pada posisi tertentu
+    override fun getItem(position: Int): Fragment {
+        return pages[position]
+    }
+
+    override fun getCount(): Int {
+        return pages.size
+    }
+
+    // judul untuk tabs
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when (position) {
+            0 -> "Last Match Tap"
+            else -> "Next Match Tap"
+        }
+    }
+}
