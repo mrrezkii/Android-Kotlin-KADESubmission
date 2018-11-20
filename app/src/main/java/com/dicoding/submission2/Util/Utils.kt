1606c0c4038d80f581fee5598db813c98da4be4d
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2018-11-20 17:26:47 +0700

    Menambahkan util

---
+++ app/src/main/java/com/dicoding/submission2/Util/Utils.kt
@@ -0,0 +1,12 @@
+package com.dicoding.submission2.Util
+
+import android.view.View
+
+fun View.visible() {
+    visibility = View.VISIBLE
+}
+
+fun View.invisible() {
+    visibility = View.INVISIBLE
+}
+
