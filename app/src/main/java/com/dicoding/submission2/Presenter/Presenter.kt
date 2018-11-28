b30ff07ce97c61ebc8ea8426ea1481452e54154b
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2018-11-28 22:13:45 +0700

    Menambahkan Presenter

---
+++ app/src/main/java/com/dicoding/submission2/Presenter/Presenter.kt
@@ -0,0 +1,48 @@
+package com.dicoding.submission2.Presenter
+
+import android.content.Context
+import android.widget.Toast
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.Volley
+import com.dicoding.submission2.Model.MatchModel
+import com.dicoding.submission2.View.ViewAdapter
+import org.json.JSONArray
+import org.json.JSONObject
+
+class Presenter(private var view: ViewAdapter, private var endpoint: String, private var context: Context) {
+    private val matchList: MutableList<MatchModel> = mutableListOf()
+    fun getData() {
+        val queue = Volley.newRequestQueue(context)
+        val stringRequestQueue = JsonObjectRequest(
+            Request.Method.GET,
+            "https://www.thesportsdb.com/api/v1/json/1/" + endpoint,
+            null,
+            Response.Listener<JSONObject> { response ->
+
+                val arr: JSONArray = response.getJSONArray("events")
+                val size = arr.length()
+                var x = 0
+                while (x < size) {
+                    val obj = arr.getJSONObject(x)
+                    matchList.add(
+                        MatchModel(
+                            obj.getString("idEvent"),
+                            obj.getString("strDate"),
+                            obj.getString("strHomeTeam"),
+                            obj.getString("strAway"),
+                            obj.getString("intHomeScore"),
+                            obj.getString("intAwayScore")
+                        )
+                    )
+                    x = x.inc()
+                }
+                view.showDataRecycler(matchList)
+            },
+            Response.ErrorListener {
+                Toast.makeText(context, it.toString(), Toast.LENGTH_SHORT).show()
+            })
+        queue.add(stringRequestQueue)
+    }
+}
