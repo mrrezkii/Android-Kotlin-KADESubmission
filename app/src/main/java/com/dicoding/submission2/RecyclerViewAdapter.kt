c889c8d86bf8240098a5c4a58f64932a681dc8aa
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2018-11-28 21:21:56 +0700

    Menambahkan view holder dan setting card view di recycler layout

--- app/src/main/java/com/dicoding/submission2/RecyclerViewAdapter.kt
+++ app/src/main/java/com/dicoding/submission2/RecyclerViewAdapter.kt
@@ -2,14 +2,17 @@ package com.dicoding.submission2
 
 import android.content.Context
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.dicoding.submission2.Model.MatchModel
+import kotlinx.android.synthetic.main.recyclerview_layout.view.*
 
 class RecyclerViewAdapter(val context: Context, private var ls: MutableList<MatchModel>) :
     RecyclerView.Adapter<ViewHolder>() {
+
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.recyclerview_layout, p0, false))
     }
 
     override fun getItemCount(): Int {
@@ -22,4 +25,16 @@ class RecyclerViewAdapter(val context: Context, private var ls: MutableList<Matc
 
 }
 
-class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    fun bind(data: MatchModel) {
+        itemView.tvDate.text = data.dateEvent
+        itemView.tvTeamKiri.text = data.strHomeTeam
+        itemView.tvSkorKiri.text = data.intHomeScore
+        itemView.tvTeamKanan.text = data.strAwayTeam
+        itemView.tvSkorKanan.text = data.intAwayScore
+        itemView.cvMatch.setOnClickListener {
+
+        }
+
+    }
+}
