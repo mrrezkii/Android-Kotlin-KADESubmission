3b983e13ff52460099ce6a58390cc3b7fdb6dce5
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2018-12-01 20:46:23 +0700

    Fungsional Tugas 2

--- app/src/main/java/com/dicoding/submission2/DetailActivity.kt
+++ app/src/main/java/com/dicoding/submission2/DetailActivity.kt
@@ -1,5 +1,59 @@
 package com.dicoding.submission2
 
+import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.dicoding.submission2.Model.DetailModel
+import com.dicoding.submission2.Model.TeamModel
+import com.dicoding.submission2.Presenter.DetailPresenter
+import com.dicoding.submission2.View.ViewDetail
+import kotlinx.android.synthetic.main.activity_detail.*
 
-class DetailActivity : AppCompatActivity()
+class DetailActivity : AppCompatActivity(), ViewDetail {
+    override fun setData(det: DetailModel, homeTeam: TeamModel, awayTeam: TeamModel) {
+        progressBar.visibility = View.GONE
+        tvDate.text = det.dateEvent
+        Glide.with(this).load(homeTeam.emblem).into(ivHome)
+        Glide.with(this).load(awayTeam.emblem).into(ivAway)
+        tvHome.text = homeTeam.name
+        tvAway.text = awayTeam.name
+        if (det.intHomeScore == "null") {
+            tvHomeScore.text = "-"
+        } else {
+            tvHomeScore.text = det.intHomeScore
+
+        }
+        if (det.intAwayScore == "null") {
+            tvAwayScore.text = "-"
+        } else {
+            tvAwayScore.text = det.intHomeScore
+
+        }
+
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        val presenter = DetailPresenter(this, intent.getStringExtra("idEvent"), this)
+        progressBar.visibility = View.VISIBLE
+        presenter.getData()
+    }
+
+    private fun setGoalText(goal: String, tv: TextView) {
+        if (goal == "null") {
+            tv.text = "-"
+        } else {
+            val goalArr = goal.split(";")
+            for (str: String in goalArr) {
+                val s = tv.text.toString() + str + "\n"
+                tv.text = s
+            }
+
+        }
+    }
+
+
+}
--- app/src/main/java/com/dicoding/submission2/Model/TeamModel.kt
+++ app/src/main/java/com/dicoding/submission2/Model/TeamModel.kt
@@ -2,9 +2,5 @@ package com.dicoding.submission2.Model
 
 data class TeamModel(
     var name: String? = null,
-    var emblem: String? = null,
-    var idTeam: String? = null,
-    var strTeam: String? = null,
-    var strDescriptionEN: String? = null,
-    var strTeamBadge: String? = null
+    var emblem: String? = null
 )
