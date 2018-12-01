e77d9fd08c67dea49bd5e7f7f2aa10a41913df13
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2018-12-01 23:15:20 +0700

    Menambahkan fragment favorite

--- app/src/main/java/com/dicoding/submission2/MyPagerAdapter.kt
+++ app/src/main/java/com/dicoding/submission2/MyPagerAdapter.kt
@@ -3,6 +3,7 @@ package com.dicoding.submission2
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
+import com.dicoding.submission2.Fragment.FavoriteFragment
 import com.dicoding.submission2.Fragment.LastMatchFragment
 import com.dicoding.submission2.Fragment.NextMatchFragment
 
@@ -14,6 +15,7 @@ class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
     init {
         pages.add(LastMatchFragment())
         pages.add(NextMatchFragment())
+        pages.add(FavoriteFragment())
     }
     // menentukan fragment yang akan dibuka pada posisi tertentu
     override fun getItem(position: Int): Fragment? {
@@ -27,8 +29,9 @@ class MyPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
     // judul untuk tabs
     override fun getPageTitle(position: Int): CharSequence? {
         return when (position) {
-            0 -> "Last MatchModel"
-            else -> "Next MatchModel"
+            0 -> "Last Match"
+            1 -> "Next Match"
+            else -> "Favorite"
         }
     }
 }
