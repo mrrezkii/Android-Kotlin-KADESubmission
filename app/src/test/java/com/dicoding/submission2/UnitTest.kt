9d434c50fcc05442f106c5fb5c3a48740ca525ce
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2018-12-02 10:47:38 +0700

    Finishing Unit Test

--- app/src/test/java/com/dicoding/submission2/UnitTest.kt
+++ app/src/test/java/com/dicoding/submission2/UnitTest.kt
@@ -1,7 +1,16 @@
 package com.dicoding.submission2
 
-import org.junit.Assert.assertEquals
+import com.dicoding.submission2.presenter.DetailPresenter
+import com.dicoding.submission2.presenter.FavoritePresenter
+import com.dicoding.submission2.presenter.MatchPresenter
+import com.dicoding.submission2.repository.DetailRepo
+import com.dicoding.submission2.repository.FavoriteRepo
+import com.dicoding.submission2.repository.MatchRepo
+import org.junit.Before
 import org.junit.Test
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -9,8 +18,41 @@ import org.junit.Test
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class UnitTest {
+    private lateinit var matchPresenter: MatchPresenter
+    private lateinit var detailPresenter: DetailPresenter
+    private lateinit var favPresenter: FavoritePresenter
+
+    @Mock
+    private lateinit var repo: MatchRepo
+    @Mock
+    private lateinit var detRepo: DetailRepo
+    @Mock
+    private lateinit var favRepo: FavoriteRepo
+
+    @Before
+    fun setUp() {
+        MockitoAnnotations.initMocks(this)
+        matchPresenter = MatchPresenter("eventspastleague.php?id=4328", repo)
+        detailPresenter = DetailPresenter("lookupevent.php?id=576504", detRepo)
+        favPresenter = FavoritePresenter(favRepo)
+    }
+
+
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun testGetMatchList() {
+        matchPresenter.getData()
+        Mockito.verify(repo).getMatch("eventspastleague.php?id=4328")
+    }
+
+    @Test
+    fun testGetTeamDetail() {
+        detailPresenter.getData()
+        Mockito.verify(detRepo).getDetail("lookupevent.php?id=576504")
+    }
+
+    @Test
+    fun testGetFavorite() {
+        favPresenter.getData()
+        Mockito.verify(favRepo).getFavorite()
     }
 }
