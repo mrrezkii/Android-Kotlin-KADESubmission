0c313d0f417a7e1841c2c5907d8a466c0251d209
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2018-12-02 10:31:44 +0700

    Merubah endpoint menjadi id event di detal presenter

--- app/src/main/java/com/dicoding/submission2/presenter/DetailPresenter.kt
+++ app/src/main/java/com/dicoding/submission2/presenter/DetailPresenter.kt
@@ -2,8 +2,8 @@ package com.dicoding.submission2.presenter
 
 import com.dicoding.submission2.repository.DetailRepo
 
-class DetailPresenter(private var endpoint: String, private var repo: DetailRepo) {
+class DetailPresenter(private var idEvent: String, private var repo: DetailRepo) {
     fun getData() {
-        repo.getDetail(endpoint)
+        repo.getDetail(idEvent)
     }
 }
