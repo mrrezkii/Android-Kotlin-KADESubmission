b5f2fedce5830a1f360998140f3db505c158e03f
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2018-12-03 19:30:43 +0700

    Bottom Nav bug

--- app/src/main/java/com/dicoding/submission2/activity/MainActivity.kt
+++ app/src/main/java/com/dicoding/submission2/activity/MainActivity.kt
@@ -5,6 +5,10 @@ import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import com.dicoding.submission2.R
+import com.dicoding.submission2.R.id.*
+import com.dicoding.submission2.fragment.FavoriteFragment
+import com.dicoding.submission2.fragment.MatchFragment
+import com.dicoding.submission2.fragment.TeamFragment
 
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +21,33 @@ class MainActivity : AppCompatActivity() {
 
         toolbar = supportActionBar!!
         val bottomNavigation: BottomNavigationView = findViewById(R.id.bn_main)
+        bottomNavigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
 
     }
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            matches_menu -> {
+                toolbar.title = "Match"
+                val fl = MatchFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.fl_container, fl).commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            teams_menu -> {
+                toolbar.title = "Team"
+                val fl = TeamFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.fl_container, fl).commit()
+                return@OnNavigationItemSelectedListener true
+            }
+            favorites_menu -> {
+                toolbar.title = "Favorite"
+                val fl = FavoriteFragment()
+                supportFragmentManager.beginTransaction().replace(R.id.fl_container, fl).commit()
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+
 }
