c4ac986c0501ea0e209a5164837719670f733db5
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2018-12-03 18:28:14 +0700

    Finishing bottom nav

--- app/src/main/java/com/dicoding/submission2/activity/MainActivity.kt
+++ app/src/main/java/com/dicoding/submission2/activity/MainActivity.kt
@@ -1,19 +1,26 @@
 package com.dicoding.submission2.activity
 
 import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.ActionBar
 import android.support.v7.app.AppCompatActivity
 import com.dicoding.submission2.R
-import com.dicoding.submission2.adapter.MyPagerAdapter
-import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var toolbar: ActionBar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        viewpager_main.adapter = MyPagerAdapter(supportFragmentManager)
-        tabs_main.setupWithViewPager(viewpager_main)
+        toolbar = supportActionBar!!
+        val bottomNavigation: BottomNavigationView = findViewById(R.id.bn_main)
+
+        /*viewpager_main.adapter = MyPagerAdapter(supportFragmentManager)
+        tabs_main.setupWithViewPager(viewpager_main)*/
+
+
     }
 }
