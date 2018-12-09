69d47a290e8a88b7ad39f2fff4531395abc7ff1f
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2018-12-09 13:29:49 +0700

    Menambahkan fitur favorite team

--- app/src/main/java/com/dicoding/submission2/activity/DetailTeamActivity.kt
+++ app/src/main/java/com/dicoding/submission2/activity/DetailTeamActivity.kt
@@ -9,11 +9,11 @@ import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
 import com.bumptech.glide.Glide
 import com.dicoding.submission2.R
 import com.dicoding.submission2.adapter.DetailTeamPagerAdapter
 import com.dicoding.submission2.database.DBHelper
-import com.dicoding.submission2.model.MatchFavoriteModel
 import com.dicoding.submission2.model.TeamFavoriteModel
 import com.dicoding.submission2.model.TeamListModel
 import com.dicoding.submission2.presenter.TeamDetailPresenter
@@ -42,6 +42,7 @@ class DetailTeamActivity : AppCompatActivity(), ViewTeamDetail {
             viewpager_main.parent.requestDisallowInterceptTouchEvent(true)
             false
         }
+        this.detail = detail
         tabs_main.setupWithViewPager(viewpager_main)
 
     }
@@ -52,16 +53,21 @@ class DetailTeamActivity : AppCompatActivity(), ViewTeamDetail {
         val teamDetailPresenter = TeamDetailPresenter(intent.getStringExtra("id"), TeamDetailRepo(this, this))
         teamDetailPresenter.getData()
         nested.isFillViewport = true
-        database.use {
-            val result = select(TeamFavoriteModel.TABLE_FAVORITE_TEAM).whereArgs(
-                "(" + TeamFavoriteModel.ID + ") = {id}",
-                "id" to intent.getStringExtra("id")
-            ).exec {
-                parseList(
-                    classParser<MatchFavoriteModel>()
-                )
+        try {
+            database.use {
+                val result = select(TeamFavoriteModel.TABLE_FAVORITE_TEAM).whereArgs(
+                    "(" + TeamFavoriteModel.ID + ") = {id}",
+                    "id" to intent.getStringExtra("id")
+                ).exec {
+                    parseList(
+                        classParser<TeamFavoriteModel>()
+                    )
+                }
+                if (!result.isEmpty()) favorite = true
+
             }
-            if (!result.isEmpty()) favorite = true
+        } catch (e: Exception) {
+            Toast.makeText(this, e.message!!, Toast.LENGTH_SHORT).show()
         }
         isAvailable()
     }
@@ -110,17 +116,22 @@ class DetailTeamActivity : AppCompatActivity(), ViewTeamDetail {
 
     private fun addFav(): Boolean {
         if (this::detail.isInitialized) {
-            database.use {
-                insert(
-                    TeamFavoriteModel.TABLE_FAVORITE_TEAM, TeamFavoriteModel.TEAM_ID to intent.getStringExtra("id"),
-                    TeamFavoriteModel.STR_TEAM to detail.name,
-                    TeamFavoriteModel.STR_TEAM_BADGE to detail.emblem,
-                    TeamFavoriteModel.STR_TEAM_DESCRIPTION to detail.desc
-                )
+            try {
+                database.use {
+                    insert(
+                        TeamFavoriteModel.TABLE_FAVORITE_TEAM, TeamFavoriteModel.TEAM_ID to intent.getStringExtra("id"),
+                        TeamFavoriteModel.STR_TEAM to detail.name,
+                        TeamFavoriteModel.STR_TEAM_BADGE to detail.emblem,
+                        TeamFavoriteModel.STR_TEAM_DESCRIPTION to detail.desc
+                    )
+                }
+            } catch (e: Exception) {
+                Toast.makeText(this, e.message!!, Toast.LENGTH_SHORT).show()
             }
             favorite = true
             return true
         }
+        Toast.makeText(this, "Not Initialized", Toast.LENGTH_SHORT).show()
         return false
     }
 
@@ -145,9 +156,9 @@ class DetailTeamActivity : AppCompatActivity(), ViewTeamDetail {
             return false
         } else if (this !is Application) {
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN_MR1) {
-                if (this is DetailActivity) {
+                if (this is DetailTeamActivity) {
                     return !this.isDestroyed
-                } else if (this is DetailActivity) {
+                } else if (this is DetailTeamActivity) {
                     return !this.isDestroyed
                 }
             }
